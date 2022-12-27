30be361326165bc04058cac1c2ca38aacf2335e7
JesperFischer <jesperfischer@outlook.dk>
2022-12-28 00:41:00 +0100

    a

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+python3 -m venv env
+
+source ./env/bin/activate
+sudo apt-get install python3-dev
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python3 scripts/Main_analysis.py
+
+deactivate
